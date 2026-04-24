42ad0838828bcf8b54c6251dc9375b02f001b6e7
Ubuntu <ubuntu@ip-172-31-30-65.ec2.internal>
2026-04-24 19:34:28 +0000

    se suben los modelos

--- xrayflares/plugin/models.py
+++ xrayflares/plugin/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Flux(models.Model):
+	date_time= models.DateTimeField("date time", primary_key=True)
+	flux = models.FloatField(default=0.0)
+
